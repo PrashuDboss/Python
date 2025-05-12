e3288ceaea773d1bb65f8270ef05eef9b3468b8b
PrashuDboss <prashanthvekatesh619@gmail.com>
2025-05-12 20:40:17 +0530

    Add files via upload

---
+++ Assignment 1 Task 2.py
@@ -0,0 +1,4 @@
+a= input('Enter your first name: ')
+b= input('Enter your last name: ')
+
+print('Hello, '+ str(a)+' ' +str(b) + '! Welcome to the Python program')
---
+++ Assignment 1.py
@@ -0,0 +1,12 @@
+#Task 1: Perform Basic Mathematical Operations
+a= int(input('Enter first number: '))
+b= int(input('Enter second number: '))
+c= a+b
+d=a-b
+e=a*b
+f=a/b
+
+print('Additon is: '+str(c))
+print('Subtraction is: '+str(d))
+print('Multiplication is: '+str(e))
+print('Division is: '+str(f))
